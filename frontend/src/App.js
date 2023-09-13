c673d82e3941d974b64fec125e29794cbe07ce04
razzivofficial <razzivofficial@gmail.com>
2023-09-13 20:45:46 +0530

    Components-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
+import React from "react";
+import MainPage from "./components/MainPage/MainPage";
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold first-letter:capitalize">
-        hello world!
-      </h1>
-      ;
+      <MainPage />
     </>
   );
 }
---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,7 @@
+export default function MainPage() {
+  return (
+    <>
+      <h1 className="font-serif font-bold text-lg">Test123</h1>
+    </>
+  );
+}
