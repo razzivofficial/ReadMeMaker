45253b74a2802063af5ce682cd2912abbb4da07e
razzivofficial <razzivofficial@gmail.com>
2023-09-15 16:22:29 +0530

    added-element-adder

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+import PreElementAdder from "../../Components/ElementAdder/PreElementAdder";
+
 export default function HomePage() {
   return (
     <div>
       <Navbar />
       <Footer />
+      <PreElementAdder />
     </div>
   );
 }
