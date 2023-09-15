2fb2d575f05648413a53b34a53ff92d84ccd99f3
razzivofficial <razzivofficial@gmail.com>
2023-09-15 11:33:20 +0530

    started-routing-pages

---
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
