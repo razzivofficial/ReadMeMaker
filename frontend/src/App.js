b2d6d16fa0a616a3b2a34a18700c7117c5814d38
Rajiv Lochan Dash <91611270+razzivofficial@users.noreply.github.com>
2023-09-15 15:52:16 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import necessary components from react-router-dom
-import UserContextProvider from "./UserContextProvider"; // Import your UserContextProvider component
-import Navbar from "./Navbar"; // Import your Navbar component
-import HomePage from "./Pages/HomePage/HomePage"; // Import your HomePage component
-
+import HomePage from "./Pages/HomePage/HomePage";
 function App() {
   return (
-    <BrowserRouter>
-      <UserContextProvider>
-        <Navbar
-          classDisplay={classDisplay}
-          setClassDisplay={setClassDisplay}
-          displayVal={displayVal}
-          setDisplayVal={setDisplayVal}
-          setLoginState={setLoginState}
-          loginState={loginState}
-        />
-        <div style={intDivStyle}>
-          <Routes>
-            {/* <Route path="/nearby" element={<NearbyMap nonceVal={nonce} />} /> */}
-            <Route
-              path="/"
-              element={<HomePage nonceVal={nonce} loginState={loginState} />}
-            />
-          </Routes>
-        </div>
-      </UserContextProvider>
-    </BrowserRouter>
+    <>
+      <HomePage />
+    </>
   );
 }
 
