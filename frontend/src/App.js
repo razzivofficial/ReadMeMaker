ab8c8ca11b7c3c81ca257770c951be3459761e74
razzivofficial <razzivofficial@gmail.com>
2023-09-17 14:57:33 +0530

    Routing-started-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import HomePage from "./Pages/HomePage/HomePage";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
-
-
 function App() {
   return (
     <>
-      <Navbar />
       <HomePage />
-
-      <Footer />
     </>
   );
 }
