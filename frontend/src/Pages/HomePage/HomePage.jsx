93d026a3cdb487ab25825911d970faa8ecd528b0
razzivofficial <razzivofficial@gmail.com>
2023-09-17 14:12:37 +0530

    Routing-started

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Navbar from "../../Components/Navbar/Navbar";
-import Footer from "../../Components/Footer/Footer";
 import ElementAdder from "../../Components/ElementAdder/ElementAdder";
 
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
       <ElementAdder />
-      <Footer />
     </div>
   );
 }
