51ab0236ee6afbd2b8c2d43d8271dbfdf9b27e93
razzivofficial <razzivofficial@gmail.com>
2023-09-19 11:02:04 +0530

    hero-section-modified

--- frontend/src/Components/HeroSection/HeroSection.jsx
+++ frontend/src/Components/HeroSection/HeroSection.jsx
@@ -1,13 +1,18 @@
 import * as React from "react";
 import { chakra, Stack, Text, Button, Box } from "@chakra-ui/react";
-import { FaGithub } from "react-icons/fa";
+import { GiCoffeeCup } from "react-icons/gi";
 
 const HeroSection = () => {
   return (
-      <Box p={{ base: 8, md: 14 }} bgColor={"whitesmoke"}
-      m={6} mt={30}>
+    <Box
+      p={{ base: 8, md: 14 }}
+      bgColor={"whitesmoke"}
+      m={4}
+      mt={{ base: 12, md: 18 }}
+    >
       <Stack direction="column" spacing={6} alignItems="center">
         <Box
+          mt={10}
           py={2}
           px={3}
           bg="teal"
@@ -18,26 +23,31 @@ const HeroSection = () => {
         >
           <Stack direction={{ base: "column", sm: "row" }}>
             <Text fontWeight="bold">Ready, Set, Build! 🚀</Text>
-            <Text>Join the Hackathon!</Text>
+            <Text>Join the Community!</Text>
           </Stack>
         </Box>
         <chakra.h1
           fontSize={{ base: "4xl", sm: "5xl" }}
           fontWeight="bold"
           textAlign="center"
-          maxW="600px"
+          maxW="800px"
         >
-                  Get started with{" "}
-                  <br />
+          Get started with ReadMeMaker
+          <br />
           <chakra.span
             color="teal"
             bg="linear-gradient(transparent 50%, #83e9e7 50%)"
           >
-            animated content
+            Drag & Drop Interface Markdown Preview Custom Templates
           </chakra.span>
         </chakra.h1>
         <Text maxW="550px" fontSize="xl" textAlign="center" color="gray.500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas error sequi autem praesentium dolores est minima iure? Ipsa ea distinctio laudantium dolorem nisi, reprehenderit facilis odio, assumenda sunt praesentium quas?
+          Crafting documentation is like painting the portrait of your project's
+          soul. Each line of code, every design choice, and every contributor's
+          effort are woven together into a tapestry that tells the story of your
+          creation. With our README.md generator, you hold the artist's brush,
+          effortlessly turning your vision into a masterpiece of clarity and
+          understanding.
         </Text>
         <Stack
           direction={{ base: "column", sm: "row" }}
@@ -55,15 +65,16 @@ const HeroSection = () => {
             Get Started
           </Button>
           <Button
-            leftIcon={<FaGithub />}
+            leftIcon={<GiCoffeeCup />}
             colorScheme="gray"
             variant="outline"
             rounded="md"
             size="lg"
             height="3.5rem"
-            fontSize="1.2rem"
+            fontSize="1rem"
+            borderColor="black.500"
           >
-            Github
+            Buy me a coffee
           </Button>
         </Stack>
       </Stack>
