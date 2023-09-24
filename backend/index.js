c363b37bf69d5e0351278f89d2a8246c5835f2ca
razzivofficial <razzivofficial@gmail.com>
2023-09-24 23:54:12 +0530

    backend-working

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://razzivofficial:rOJUw5Dr785JZCYC@clusermd.pfxgblf.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Connected to mongo DB");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const port = 8080;
-mongoose.connect(
-  "mongodb+srv://razzivofficial:rOJUw5Dr785JZCYC@clusermd.pfxgblf.mongodb.net/?retryWrites=true&w=majority"
-);
 app.listen(port, () => {
   console.log("Har har mahadev");
 });
+
+module.exports = connectDB;
