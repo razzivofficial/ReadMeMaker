58114d75ed5c2b529bab203a71d870827ac89b63
razzivofficial <razzivofficial@gmail.com>
2023-09-25 08:32:13 +0530

    writing-schema

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const BASE_URL =
+  "mongodb+srv://razzivofficial:rOJUw5Dr785JZCYC@clusermd.pfxgblf.mongodb.net/myDb";
 const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://razzivofficial:rOJUw5Dr785JZCYC@clusermd.pfxgblf.mongodb.net/myDb"
-    );
-    console.log("Connected to mongo DB");
-  } catch (error) {
-    console.error(error);
-  }
+  await mongoose.connect(BASE_URL);
+  // try {
+  //   await mongoose.connect(BASE_URL);
+  //   console.log("Connected to mongo DB");
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 
 const port = 8080;
