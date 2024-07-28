8c1f0785a95be5ae06817454cd753a19c1422638
razzivofficial <razzivofficial@gmail.com>
2024-07-28 12:11:58 +0530

    Fix port conflict and update vercel configuration

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/users", User); // Updated route
+app.use("/api/users", User);
 
-const port = process.env.PORT || 5000; // Use the PORT environment variable provided by Vercel
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
