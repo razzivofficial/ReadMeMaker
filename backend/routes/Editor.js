bae303965ad28595879008c7ede90d33f71d85cc
tushargandhi77 <gandhitushar418@gmai.com>
2024-08-10 18:17:18 +0530

    editor card like

--- backend/routes/Editor.js
+++ backend/routes/Editor.js
@@ -6,12 +6,11 @@ const editordetails = require('../controller/Editor.controller')
 router.post('/addeditor',editordetails.addEditor)
 router.get('/getalleditor',editordetails.fetcheditors)
 router.get('/geteditorbyemail/:email',editordetails.fetchbyemail)
-router.patch('/upvoteplus/:id',editordetails.upvoteEditorplus)
-router.patch('/upvoteminus/:id',editordetails.upvoteEditorminus)
-router.patch('/downvoteplus/:id',editordetails.downvoteEditorplus)
-router.patch('/downvoteminus/:id',editordetails.downvoteEditorminus)
 
 
 router.patch('/upvoteeditor',editordetails.upvoteEditor)
 router.patch('/downvoteeditor',editordetails.downvoteEditor)
+
+router.get('/checkvotestatus', editordetails.checkVoteStatus);
+
 module.exports = router;
