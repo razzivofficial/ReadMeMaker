d819f6ee5fc4e362e5a6a4aa8692bd0358994397
tushargandhi77 <gandhitushar418@gmai.com>
2024-08-13 17:40:06 +0530

    edit button prject

--- frontend/src/utils/emailUtils.js
+++ frontend/src/utils/emailUtils.js
@@ -1,18 +1,16 @@
-// Function to encode the email to base64
 export function encodeEmail(email) {
-    if (typeof email !== 'string') {
-    //   console.error(`encodeEmail expects a string but received ${typeof email}`);
-      return null; // Return null or handle it as per your need
-    }
-    return btoa(unescape(encodeURIComponent(email)));
+  if (typeof email !== 'string') {
+  //   console.error(`encodeEmail expects a string but received ${typeof email}`);
+    return null; // Return null or handle it as per your need
   }
-  
-  // Function to decode the email from base64
-  export function decodeEmail(encodedEmail) {
-    if (typeof encodedEmail !== 'string') {
-    //   console.error(`decodeEmail expects a string but received ${typeof encodedEmail}`);
-      return null; // Return null or handle it as per your need
-    }
-    return decodeURIComponent(escape(atob(encodedEmail)));
+  return btoa(unescape(encodeURIComponent(email)));
+}
+
+// Function to decode the email from base64
+export function decodeEmail(encodedEmail) {
+  if (typeof encodedEmail !== 'string') {
+  //   console.error(`decodeEmail expects a string but received ${typeof encodedEmail}`);
+    return null; // Return null or handle it as per your need
   }
-  
+  return decodeURIComponent(escape(atob(encodedEmail)));
+}
