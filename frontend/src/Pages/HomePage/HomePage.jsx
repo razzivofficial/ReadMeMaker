0ccdcb043bea8da69eb2b4bc1ee58b5dad106482
razzivofficial <razzivofficial@gmail.com>
2024-08-17 18:45:52 +0530

    doing-seo-removed-templates

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import HeroSection from "../../Components/HeroSection/HeroSection";
 import Loader from "../../Components/Loader/Loader";
+import { Helmet } from "react-helmet";
 
 export default function HomePage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -14,6 +15,17 @@ export default function HomePage() {
 
   return (
     <>
+      <Helmet>
+        <title>ReadMeMaker - Best README Crafter</title>
+        <meta
+          name="description"
+          content="Easily create professional README files with our tool. Perfect for developers who want to document their projects effortlessly."
+        />
+        <meta
+          name="keywords"
+          content="readme, markdown, readmemaker, create readme, readme templates, markdown templates"
+        />
+      </Helmet>
       {isLoading ? (
         <Loader />
       ) : (
